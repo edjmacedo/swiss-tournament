013ed824d38b97c9d26a94b60ca5c557e14d79e4
Edvan Macedo <edvan.macedo.jr@gmail.com>
2017-04-24 21:58:30 -0400

    feat: Implement sql tables for swiss tournament
    Create two tables, playes that will score all playes in champioship and
    another with the matches informations such as winners and losers

--- tournament.sql
+++ tournament.sql
@@ -1,9 +1,15 @@
 -- Table definitions for the tournament project.
---
--- Put your SQL 'create table' statements in this file; also 'create view'
--- statements if you choose to use it.
---
--- You can write comments in this file by starting them with two dashes, like
--- these lines here.
 
+-- Players table
+CREATE TABLE players (
+    p_id SERIAL primary key,
+    p_name text,
+    p_color text
+);
 
+-- Matches table
+CREATE TABLE matches (
+    m_id SERIAL primary key,
+    m_winner SERIAL references players(p_id),
+    m_loser SERIAL references players(p_id)
+);
